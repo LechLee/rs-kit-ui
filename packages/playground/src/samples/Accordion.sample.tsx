897b4580def21aa42caf054c319515bfb221b574
Chris Lee <chrisyeehng@gmail.com>
2025-04-03 15:59:29 +0800

    Fix Accordion

--- packages/playground/src/samples/Accordion.sample.tsx
+++ packages/playground/src/samples/Accordion.sample.tsx
@@ -1,4 +1,4 @@
-import { Accordion } from 'ui-kit'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from 'ui-kit'
 import { ComponentDoc } from '../components/ComponentDoc'
 
 export default function AccordionSample() {
@@ -8,18 +8,18 @@ export default function AccordionSample() {
 			description="A vertically stacked set of interactive headings that each reveal a section of content."
 			component={
 				<Accordion type="single" collapsible className="w-full">
-					<div>
-						<h3 className="font-medium">Is it accessible?</h3>
-						<div className="pt-2">Yes. It adheres to the WAI-ARIA design pattern.</div>
-					</div>
-					<div className="mt-4">
-						<h3 className="font-medium">Is it styled?</h3>
-						<div className="pt-2">Yes. It comes with default styles that matches the other components' aesthetic.</div>
-					</div>
-					<div className="mt-4">
-						<h3 className="font-medium">Is it animated?</h3>
-						<div className="pt-2">Yes. It's animated by default, but you can disable it if you prefer.</div>
-					</div>
+					<AccordionItem value="item-1">
+						<AccordionTrigger>Is it accessible?</AccordionTrigger>
+						<AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value="item-2">
+						<AccordionTrigger>Is it styled?</AccordionTrigger>
+						<AccordionContent>Yes. It comes with default styles that matches the other components&apos; aesthetic.</AccordionContent>
+					</AccordionItem>
+					<AccordionItem value="item-3">
+						<AccordionTrigger>Is it animated?</AccordionTrigger>
+						<AccordionContent>Yes. It's animated by default, but you can disable it if you prefer.</AccordionContent>
+					</AccordionItem>
 				</Accordion>
 			}
 		/>
